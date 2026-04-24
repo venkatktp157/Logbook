8ba8d73ae8d6021c4b9e0b2262a591c20d752deb
venkatktp157 <vatsal157@gmail.com>
2026-04-24 11:22:46 +0530

    modified sterntube column page3

--- pages_content.py
+++ pages_content.py
@@ -73,7 +73,7 @@ def page_two():
 
 def page_three():
     st.title("📈 Main Engine Data 3")
-    col1, col2, col3 = st.columns(3)    
+    col1, col2, col3, col4 = st.columns(4)    
 
     with col1:
         st.subheader("Cooling & Fuel")
@@ -106,6 +106,9 @@ def page_three():
         st.number_input("ME FO Filter Backwash Counter", key="MEFOfilter_backwash_ctr", step=1) 
         st.number_input("AE FO Filter Backwash Counter", key="AEFOfilter_backwash_ctr", step=1)
         st.number_input("SternTube Gravity Tank Level (mm)", key="ST_gravtk_level", step=0.01)
+
+    with col4:
+        st.subheader("Stern Tube & Shaft")
         st.number_input("SternTube Aft Seal Tank Level (mm)", key="ST_aftsealtk_level_", step=0.01)
         st.number_input("SternTube Fwd Seal Tank Level (mm)", key="ST_fwdsealtk_level", step=0.01)
         st.number_input("SternTube Oil In Press (bar)", key="ST_Oilin_press", step=0.01)
